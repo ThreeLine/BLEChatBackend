80ea7d2afbe1c56183bd7324fd75a59dd635dc78
Ocean <ocean.yang@ihunuo.com>
2015-11-14 23:12:49 +0800

    finish load user info

--- chat/src/main/java/com/fnx/webapp/controller/open/user/UserController.java
+++ chat/src/main/java/com/fnx/webapp/controller/open/user/UserController.java
@@ -1,8 +1,5 @@
 package com.fnx.webapp.controller.open.user;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fnx.db.entity.tenant.TenantDBVO;
 import com.fnx.db.entity.user.UserDBVO;
-import com.fnx.repository.mongo.tenant.TenantAutoRepo;
 import com.fnx.repository.mongo.user.UserAutoRepo;
 import com.fnx.webapp.model.common.ResponseModel;
-import com.fnx.webapp.model.saas.TenantModel;
 import com.fnx.webapp.model.user.UserModel;
 import com.fnx.webapp.util.WebConstants;
 
@@ -51,25 +45,22 @@ public class UserController {
 		userAutoRepo.save(userDBVO);
 		return resModel;
 	}
-	
-	
-	@Autowired
-	private TenantAutoRepo tenantAutoRepo;
 
-	@RequestMapping(value = "/info", method = RequestMethod.GET)
-	public ResponseModel loadTenants() {
+	@RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
+	public ResponseModel loadPersion(@PathVariable String id) {
 		ResponseModel resModel = new ResponseModel();
-		List<TenantDBVO> tenantDBVOs = tenantAutoRepo.findAll();
-		if (!tenantDBVOs.isEmpty()) {
-			List<TenantModel> tenants = new ArrayList<TenantModel>();
-			for (TenantDBVO tenantDBVO : tenantDBVOs) {
-				TenantModel tenantModel = new TenantModel();
-				tenantModel.setId(tenantDBVO.getId());
-				tenantModel.setName(tenantDBVO.getFullName());
-				tenants.add(tenantModel);
-			}
-			resModel.setData(tenants);
-		}
+		UserDBVO userDBVO = userAutoRepo.findOne(id);
+		if (userDBVO == null) {
+			resModel.setCode(WebConstants.RESPONSE_CODE_RESOURCE_NOT_EXIST);
+			return resModel;
+		}		
+		UserModel model = new UserModel();
+		model.setAge(userDBVO.getAge());
+		model.setId(userDBVO.getId());
+		model.setImagePath(userDBVO.getImagePath());
+		model.setName(userDBVO.getName());
+		model.setSex(userDBVO.getSex());
+		resModel.setData(model);
 		return resModel;
-	}	
+	}
 }
--- chat/src/test/java/com/fnx/webapp/controller/open/user/UserControllerTest.java
+++ chat/src/test/java/com/fnx/webapp/controller/open/user/UserControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fnx.db.entity.user.UserDBVO;
@@ -41,5 +42,27 @@ public class UserControllerTest extends BaseControllerTestCase {
 		UserDBVO customerDBVO = userAutoRepo.findOne("555ff106d4c6394ac1f87688");
 		
 		Assert.assertEquals("788.jpg", customerDBVO.getImagePath());
+	}
+	
+	private void init_loadPersion() {
+		UserDBVO userDBVO = new UserDBVO();
+		userDBVO.setAge(18);
+		userDBVO.setId("555ff106d4c6394ac1f87688");
+		userDBVO.setName("Ocean");
+		userDBVO.setSex(UserDBVO.SEX_MALE);
+		userDBVO.setImagePath("788.jpg");
+		this.basicMongoTemplate.insert(userDBVO);		
+	}
+	
+	@Test
+	public void loadPersion() throws Exception {
+		init_loadPersion();
+		ResultActions actions = this.mockMvc.perform(MockMvcRequestBuilders.get("/open/person/555ff106d4c6394ac1f87688.json"));
+		actions.andExpect(MockMvcResultMatchers.jsonPath("code").value(WebConstants.RESPONSE_CODE_SUCCESS));
+		actions.andExpect(MockMvcResultMatchers.jsonPath("data.name").value("Ocean"));
+		actions.andExpect(MockMvcResultMatchers.jsonPath("data.age").value(18));
+		actions.andExpect(MockMvcResultMatchers.jsonPath("data.id").value("555ff106d4c6394ac1f87688"));
+		actions.andExpect(MockMvcResultMatchers.jsonPath("data.sex").value(UserDBVO.SEX_MALE));
+		actions.andExpect(MockMvcResultMatchers.jsonPath("data.imagePath").value("788.jpg"));
 	}
 }
