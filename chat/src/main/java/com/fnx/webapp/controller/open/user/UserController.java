ebafe11ead50776a83c3d8c1cec441a1476b7a92
Ocean <ocean.yang@ihunuo.com>
2015-11-14 18:40:29 +0800

    update

--- chat/src/main/java/com/fnx/webapp/controller/open/user/UserController.java
+++ chat/src/main/java/com/fnx/webapp/controller/open/user/UserController.java
@@ -1,5 +1,8 @@
 package com.fnx.webapp.controller.open.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fnx.db.entity.tenant.TenantDBVO;
 import com.fnx.db.entity.user.UserDBVO;
+import com.fnx.repository.mongo.tenant.TenantAutoRepo;
 import com.fnx.repository.mongo.user.UserAutoRepo;
 import com.fnx.webapp.model.common.ResponseModel;
+import com.fnx.webapp.model.saas.TenantModel;
 import com.fnx.webapp.model.user.UserModel;
 
 @RestController
@@ -31,4 +37,25 @@ public class UserController {
 		userAutoRepo.save(userDBVO);
 		return resModel;
 	}
+	
+	
+	@Autowired
+	private TenantAutoRepo tenantAutoRepo;
+
+	@RequestMapping(value = "/info", method = RequestMethod.GET)
+	public ResponseModel loadTenants() {
+		ResponseModel resModel = new ResponseModel();
+		List<TenantDBVO> tenantDBVOs = tenantAutoRepo.findAll();
+		if (!tenantDBVOs.isEmpty()) {
+			List<TenantModel> tenants = new ArrayList<TenantModel>();
+			for (TenantDBVO tenantDBVO : tenantDBVOs) {
+				TenantModel tenantModel = new TenantModel();
+				tenantModel.setId(tenantDBVO.getId());
+				tenantModel.setName(tenantDBVO.getFullName());
+				tenants.add(tenantModel);
+			}
+			resModel.setData(tenants);
+		}
+		return resModel;
+	}	
 }
